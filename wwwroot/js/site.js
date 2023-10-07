c6bd4b0172b15183e3708e5ecea2fbbf75bf293a
Rafael Louro <rafael@code21.com.br>
2023-10-07 00:57:56 -0300

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $("#dateRangeInput").daterangepicker({
+        opens: 'left',
+        drops: 'down',
+        autoApply: true,
+        locale: {
+            format: 'DD/MM/YYYY',
+            separator: ' - ',
+            customRangeLabel: 'Custom',
+            daysOfWeek: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+            monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+        }
+    });
+});
+
+function clearForm() {
+    $("#formCustomerFilter")[0].reset();
+}
+
+function filterCustomers() {
+    $("#customerFilter").collapse("hide");
+}
